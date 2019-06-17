dc7a3b9aaf269c55f51402b6d6eeac0db9333970
Touhidul Shawan <touhidulislamshaon@gmail.com>
2019-06-18 03:58:21 +0600

    Updates

---
+++ precache-manifest.50b7ec1a34c3787b0ddcbdf25c546356.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03fb5ccddb35101c484eca1f190782e",
+    "url": "/temperature-calc/index.html"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/temperature-calc/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "50df88fc19ec8be0665f",
+    "url": "/temperature-calc/static/js/main.9942c56e.chunk.js"
+  },
+  {
+    "revision": "33607233560693fd1a4b",
+    "url": "/temperature-calc/static/js/runtime~main.82bcb723.js"
+  }
+]);
